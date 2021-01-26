c7091adc8a031c46240c27ed753b6b0d2b7ad23c
Schuler, Simon <simonpeter.schuler@soptim.de>
2021-01-26 16:41:40 +0100

    submissioncount

--- GroupPrio/src/app/entities/WeightedProject.ts
+++ GroupPrio/src/app/entities/WeightedProject.ts
@@ -1,9 +1,11 @@
 export class WeightedProject{
 title: string;
 weightedItems: Array<string> = [];
+submissionCount: number;
 
-constructor(title:string, weightedItems:Array<string>){
-this.title=title;
-this.weightedItems=weightedItems;
+constructor(title:string, weightedItems:Array<string>, submissionCount:number){
+    this.title = title;
+    this.weightedItems = weightedItems;
+    this.submissionCount = submissionCount;
 }
 }
